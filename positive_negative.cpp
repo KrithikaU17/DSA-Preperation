988e57b4b370eb9d9a3cc44144ac17f1fb25e2a0
Krithika <krithika.uthaya@gmail.com>
2026-07-25 16:03:28 +0530

    updates of day2

---
+++ positive_negative.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x>0)
+    {
+        cout<<"positive";
+    }
+    else if(x==0)
+    {
+        cout<<"it is zero";
+    }
+    else
+    {
+        cout<<"negative";
+    }
+    
+    return 0;
+}
