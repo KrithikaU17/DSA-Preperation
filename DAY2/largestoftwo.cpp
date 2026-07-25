a8d2529c1cdefe1dd36fcddd41854fc4743729cb
Krithika <krithika.uthaya@gmail.com>
2026-07-25 19:36:01 +0530

    Fix DAY2 folder

---
+++ DAY2/largestoftwo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if(x>y)
+    {
+        cout<<x<<" is larger";
+
+    }
+    else{
+        cout<<y<<" is larger";
+    }
+    return 0;
+}
